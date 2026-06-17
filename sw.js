1b94451ac8a4dba9e501ba54673f53e2119341ec
asvacubing <geniusaprameya@gmail.com>
2026-06-16 22:46:24 -0400

    ui glitch fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js — Sub-X OS service worker — VERSION: v24
-const CACHE = 'subx-v24';
+// sw.js — Sub-X OS service worker — VERSION: v25
+const CACHE = 'subx-v25';
 
 const ASSETS = [
   './', './index.html', './manifest.json',
